90e41b5c58cce7620ca49ef4641b973b89508cdf
annapureddyameenakshidevi-sys <annapureddyameenakshidevi@gmail.com>
2026-07-17 14:39:02 +0530

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,1 +1,27 @@
+from math import gcd
 
+p = 3
+q = 11
+m = 4
+
+n = p * q
+phi = (p - 1) * (q - 1)
+
+e = 3
+
+for i in range(1, phi):
+    if (e * i) % phi == 1:
+        d = i
+        break
+
+c = pow(m, e, n)
+decrypted = pow(c, d, n)
+
+print("p =", p)
+print("q =", q)
+print("n =", n)
+print("phi =", phi)
+print("e =", e)
+print("d =", d)
+print("Encrypted message =", c)
+print("Decrypted message =", decrypted)
