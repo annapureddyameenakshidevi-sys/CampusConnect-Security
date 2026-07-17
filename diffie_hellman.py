60efaedf139d71f709c88cc633649463655d0e1c
annapureddyameenakshidevi-sys <annapureddyameenakshidevi@gmail.com>
2026-07-17 14:39:26 +0530

    Update diffie_hellman.py

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -1,1 +1,21 @@
+p = 29
+g = 2
 
+a = 5
+b = 12
+
+A = pow(g, a, p)
+B = pow(g, b, p)
+
+alice_key = pow(B, a, p)
+bob_key = pow(A, b, p)
+
+print("Alice Public Key:", A)
+print("Bob Public Key:", B)
+print("Alice Shared Key:", alice_key)
+print("Bob Shared Key:", bob_key)
+
+if alice_key == bob_key:
+    print("Shared keys match")
+else:
+    print("Shared keys do not match")
